b1592024c9086cb68433964490b6b24285e2701d
Kgosi Tumi Ramotadima <kgosi95ramotadima@gmail.com>
2023-05-11 20:26:14 +0100

    PreventBufferOverflow.java

--- src/PreventBufferOverflow.java
+++ src/PreventBufferOverflow.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class PreventBufferOverflow {
+import java.util.Scanner;
+
+public class PreventBufferOverflow {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a password (up to 20 characters): ");
+        String password = scanner.nextLine();
+
+        // Validate the input to prevent buffer overflow
+        if (password.length() > 20) {
+            System.out.println("Error: Input exceeds maximum length of 20 characters.");
+            return; // Exit the program if input is invalid
+        }
+
+        System.out.println("Your password is, " + password + "!");
+        scanner.close();
+    }
 }
